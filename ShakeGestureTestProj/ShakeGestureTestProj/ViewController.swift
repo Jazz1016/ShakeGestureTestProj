1055b68b967ca56556a86517b29c1e7847f53473
James L <59751464+Jazz1016@users.noreply.github.com>
2021-07-29 12:09:06 -0600

    crystal ball svg added

--- ShakeGestureTestProj/ShakeGestureTestProj/ViewController.swift
+++ ShakeGestureTestProj/ShakeGestureTestProj/ViewController.swift
@@ -6,16 +6,24 @@
 //
 
 import UIKit
+import AVFoundation
 
 // 1. VC needs to be the first responder
 // 2. VC needs to be a able to become the 1st responder
 // 3. Detect shake
 
 class ViewController: UIViewController {
+    // MARK: - Outlets
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var smokeView: UIView!
+    @IBOutlet weak var crystalBallImageView: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBlue
+        colorView.backgroundColor = .systemOrange
+        smokeView.backgroundColor = .purple
         becomeFirstResponder()
     }
     
@@ -36,6 +44,19 @@ class ViewController: UIViewController {
 //            alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
 //            present(alert, animated: true)
         }
+    }
+    
+    func playVideo(){
+        guard let path = Bundle.main.path(forResource: "space", ofType: "mp4") else {return}
+        
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = view.bounds
+        playerLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(playerLayer)
+        player.volume = 0
+        
+        player.play()
     }
     
 }//End of class
